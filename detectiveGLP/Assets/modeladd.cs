b1835bb03593bfdeca8bc1b68b692937d14a012f
Sai Ruthvik Thandayam <f2013489@goa.bits-pilani.ac.in>
2019-05-26 12:53:09 -0700

    c

---
+++ detectiveGLP/Assets/modeladd.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class modeladd : MonoBehaviour
+{
+    public GameObject jar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void showjar()
+    {
+        jar.SetActive(true);
+    }
+}
