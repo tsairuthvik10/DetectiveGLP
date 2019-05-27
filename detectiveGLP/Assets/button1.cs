54af71788b5d5494ad9a1dc45c4d9778897277f7
Sai Ruthvik Thandayam <f2013489@goa.bits-pilani.ac.in>
2019-05-26 23:39:17 -0700

    d

---
+++ detectiveGLP/Assets/button1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class button1 : MonoBehaviour
+{
+    public Text firstnum;
+    public Text secondnum;
+    public Text thirdnum;
+    public int num;
+    public void numplace()
+    {
+        num = 3;
+
+    }
+}
---
+++ detectiveGLP/Assets/numpadScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class numpadScript : MonoBehaviour
+{
+    public GameObject clue;
+    public GameObject partition;
+    bool isOK = false;
+    public float destime;
+
+    [SerializeField]
+    Text inputField;
+
+    string inputString;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(inputString == "359" && isOK == true)
+        {
+            destime += Time.deltaTime;
+            clue.SetActive(true);
+            partition.SetActive(false);
+            if (destime > 1.0f)
+            {
+                Destroy(gameObject);
+            }
+
+        }
+
+        if(inputString != "359" && isOK == true)
+        {
+           destime += Time.deltaTime;
+            if (destime > 1.0f)
+            {
+                //explosion
+                Application.Quit();
+            }
+        }
+    }
+
+    public void buttonPressed()
+    {
+        string buttonValue = EventSystem.current.currentSelectedGameObject.name;
+        inputString += buttonValue;
+        inputField.text = inputString;
+        
+    }
+
+    public void okpressed()
+    {
+        isOK = true;
+    }
+}
